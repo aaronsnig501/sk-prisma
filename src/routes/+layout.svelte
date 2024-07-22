2456113879292cfa6e80f3fe3cf3b50a811a2d67
Aaron Sinnott <aaronsinnott93@gmail.com>
2024-07-22 12:14:03 +0100

    Tidy up styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,6 @@
   import "$lib/i18n"
   import { locale, waitLocale } from "svelte-i18n"
   import type { LayoutLoad } from "./$types"
-	import { isLocaleLoaded } from '$lib/i18n';
   import { _, locales, isLoading } from "svelte-i18n"
 
   let userLanguage: string = "en"
@@ -25,20 +24,21 @@
 
 {#if !$isLoading}
 <div class="container">
-  {#if $isLocaleLoaded}
-  <hgroup>
-    <h1><a href="/">{$_("layout.hgroup.h1")}</a></h1>
-    <h2>{$_("layout.hgroup.h2")}</h2>
-  </hgroup>
+  <nav>
+    <ul>
+      <li><strong><a href="/">Articles</a></strong></li>
+    </ul>
 
-  <select {userLanguage} on:change={handleLocaleChange}>
-    {#each $locales as locale, i}
-      <option value={locale}>{locale.toUpperCase()}</option>
-    {/each}
-  </select>
-  {:else}
-    <div class="container__content">Locale initializing...</div>
-  {/if}
+    <ul>
+      <li>
+        <select {userLanguage} on:change={handleLocaleChange}>
+          {#each $locales as locale, i}
+            <option value={locale}>{locale.toUpperCase()}</option>
+          {/each}
+        </select>
+      </li>
+    </ul>
+  </nav>
 
   <slot />
 </div>
