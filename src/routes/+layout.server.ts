abbabaaa661ce3318350673e1c13c57c62c3b567
Aaron Sinnott <aaronsinnott93@gmail.com>
2024-07-30 19:19:07 +0100

    General tidy up

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,4 @@
-import { fail, redirect } from "@sveltejs/kit"
 import type { PageServerLoad } from "./$types"
-import { lucia } from "$lib/server/auth"
 
 export const load: PageServerLoad = async ({ locals }) => {
   return { user: locals.user }
